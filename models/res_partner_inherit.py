efbdeb9ac1734d6f57864ce438166eff66f2c6eb
unknown <zayasd647@gmail.com>
2026-04-09 09:16:42 -0600

    Codigo consecutivo

--- models/res_partner_inherit.py
+++ models/res_partner_inherit.py
@@ -1,7 +1,20 @@
 from odoo import models, fields, api, _
 from datetime import date
+from typing import Union
 
 class ResPartnerInherit(models.Model):
     _inherit = 'res.partner'
 
-    code_partner = fields.Char(string="Codigo de contacto",store=True)
+    code_partner = fields.Char(string="Codigo de contacto", readonly=True, copy=False, index=True)
+    
+    @api.model
+    def create(self, vals_list: Union[dict, list]):
+        # Si viene un solo dict, convertirlo en lista
+        if isinstance(vals_list, dict):
+            vals_list = [vals_list]
+
+        for vals in vals_list:
+            if not vals.get('code_partner'):
+                vals['code_partner'] = self.env['ir.sequence'].next_by_code('res.partner.code') or '0000'
+
+        return super().create(vals_list)
