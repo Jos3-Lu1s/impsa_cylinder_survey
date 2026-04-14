98fefb0432e049334c9d31e358e87297b2db3c9c
JoseLuisTK <5joselbautistac5@gmail.com>
2026-04-14 16:16:12 -0600

    [IMP] impsa_cylinder_survey: onWillUpdateProps

--- static/src/components/dynamic_quantity_select/dynamic_quantity_select.js
+++ static/src/components/dynamic_quantity_select/dynamic_quantity_select.js
@@ -1,13 +1,28 @@
 /** @odoo-module **/
 import { registry } from "@web/core/registry";
 import { standardFieldProps } from "@web/views/fields/standard_field_props";
-import { Component } from "@odoo/owl";
+import { Component, onWillUpdateProps  } from "@odoo/owl";
 
 export class DynamicQuantitySelect extends Component {
     static template = "impsa_cylinder_survey.DynamicQuantitySelect";
     static props = {
         ...standardFieldProps,
     };
+
+    setup() {
+        // Monitoreamos cambios en los props para validar el valor actual
+        onWillUpdateProps((nextProps) => {
+            const newMax = nextProps.record.data.group_quantity || 0;
+            const currentValue = nextProps.record.data[this.props.name];
+            
+            // Si el valor actual es mayor al nuevo máximo, reseteamos a 1 o 0
+            if (currentValue > newMax && newMax > 0) {
+                this.props.record.update({ [this.props.name]: 1 });
+            } else if (newMax === 0) {
+                this.props.record.update({ [this.props.name]: 0 });
+            }
+        });
+    }
 
     /**
      * Genera dinámicamente las opciones basándose en el group_quantity
