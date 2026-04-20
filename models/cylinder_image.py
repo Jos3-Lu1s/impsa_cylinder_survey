f0f07c2798056b538874e2ec5bb7100ac0c8b8c6
JoseLuisTK <5joselbautistac5@gmail.com>
2026-04-19 19:38:04 -0600

    [IMP] impsa_cylinder_survey: 19042026

--- models/cylinder_image.py
+++ models/cylinder_image.py
@@ -25,10 +25,6 @@ class CylinderImage(models.Model):
         store=True,
         index=True,
     )
-
-    badge_ident_name=fields.Char(
-        string="Nombre identificador imagen"
-    )
     
     cylinder_number = fields.Integer(
         string="Número de Cilindro", 
