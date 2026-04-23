293419fc57c57cdb4986bf512a2d4f618ac5d6da
JoseLuisTK <5joselbautistac5@gmail.com>
2026-04-23 09:33:02 -0600

    [FIX] impsa_cylinder_survey: resolve partner code

--- models/res_partner_inherit.py
+++ models/res_partner_inherit.py
@@ -19,6 +19,17 @@ class ResPartnerInherit(models.Model):
 
         return super().create(vals_list)
     
+    @api.depends('is_company', 'name', 'parent_id.display_name', 'type', 'company_name', 'code_partner')
+    def _compute_display_name(self):
+        # 1. Ejecutar el comportamiento estándar primero
+        super()._compute_display_name()
+        
+        # 2. Recorrer los registros para inyectar el código
+        for partner in self:
+            if partner.code_partner:
+                # Modificamos el display_name para incluir el código entre corchetes
+                partner.display_name = f"[{partner.code_partner}] {partner.display_name}"
+
     @api.model
     def _search_display_name(self, operator, value): # type: ignore
         # obtenemos primero el dominio nativo
